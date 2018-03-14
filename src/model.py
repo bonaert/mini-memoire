fd29c8272f1d543b6c0780f8ac7ad372b6ae5a0b
harokb <harokb@gmail.com>
2018-03-14 18:40:55 +0100

    Now using Cohn Kanade Plus

--- src/model.py
+++ src/model.py
@@ -20,7 +20,7 @@ import utils
 
 K.set_image_data_format('channels_last')
 
-EMOTIONS = [
+JAFFE_EMOTIONS = [
     'AN',  # ANGER
     'DI',  # DISGUST,
     'FE',  # FEAR
@@ -29,14 +29,29 @@ EMOTIONS = [
     'SA',  # SADNESS
     'SU'  # SURPRISE
 ]
-NUM_EMOTIONS = len(EMOTIONS)
-CODED_EMOTIONS = [np_utils.to_categorical(i, NUM_EMOTIONS) for i in range(NUM_EMOTIONS)]
+JAFFE_NUM_EMOTIONS = len(JAFFE_EMOTIONS)
+JAFFE_CODED_EMOTIONS = [np_utils.to_categorical(i, JAFFE_NUM_EMOTIONS) for i in range(JAFFE_NUM_EMOTIONS)]
 
+CK_EMOTIONS = [
+    'ANGER',
+    'CONTEMPT',
+    'DISGUST',
+    'FEAR',
+    'HAPPY',
+    'SADNESS',
+    'SUPRISE'
+]
+CK_NUM_EMOTIONS = len(CK_EMOTIONS)
+CK_CODED_EMOTIONS = [np_utils.to_categorical(i, CK_NUM_EMOTIONS) for i in range(CK_NUM_EMOTIONS)]
+
+
+# TODO: CK and JAFFE have the same amount of labels, so I use the JAFFE coded emotions everywhere
+# even though it's a bit hackish
 
 def getMode(classifier_predictions_list):
     counts = sum(classifier_predictions_list)
     maxCountIndex = [np.argmax(row) for row in counts]
-    oneHotModes = np.array([CODED_EMOTIONS[i] for i in maxCountIndex])
+    oneHotModes = np.array([JAFFE_CODED_EMOTIONS[i] for i in maxCountIndex])
     return oneHotModes
 
 
@@ -54,7 +69,7 @@ class EnsembleClassifier:
         for classifier in self.classifiers:
             classifierPredictions = classifier.predict(X)
             oneHotPredictions = [
-                CODED_EMOTIONS[np.argmax(classifierPrediction)] for
+                JAFFE_CODED_EMOTIONS[np.argmax(classifierPrediction)] for
                 classifierPrediction in classifierPredictions
             ]
             predictions.append(oneHotPredictions)
@@ -88,15 +103,15 @@ def generateCNNParameters(random_generator):
     - The pool size of the MaxPooling Layer (if there is one)
     """
     # numLayers = random_generator.choice([2, 3, 4, 5, 6])
-    numLayers = random_generator.choice([2])
+    numLayers = random_generator.randint(2, 4)
     layersInfos = []
     for i in range(numLayers):
         # minNumKernels = 20 if i == 0 else layersInfos[-1].numKernels
         layerInfo = LayerInfo(
-            numKernels=random_generator.randint(10, 20),
+            numKernels=random_generator.randint(10, 100),
             kernelSize=random_generator.randint(2, 6),
             # hasPool=random_generator.choice([True, False]),
-            hasPool=random_generator.choice([True]),
+            hasPool=True,  # As specified by article, in part III.C
             poolSize=random_generator.choice([2, 4]))
         layersInfos.append(layerInfo)
 
@@ -109,7 +124,6 @@ class CNN:
 
         self.model = Sequential()
         self.layersInfo = generateCNNParameters(random_generator)
-        print(self.layersInfo)
 
         for (layerNum, layer) in enumerate(self.layersInfo):
             if layerNum == 0:
@@ -153,7 +167,7 @@ class CNN:
 EsnConfiguration = namedtuple("EsnConfiguration", ['reservoirSize', 'spectralRadius', 'degreeSparsity'])
 
 
-def createESN(inputSize, reservoirSize=None, spectralRadius=None,
+def createESN(inputSize, outputSize, reservoirSize=None, spectralRadius=None,
               degreeSparsity=None, randomState=None):
     if randomState is None:
         randomState = np.random.RandomState(42)
@@ -170,25 +184,23 @@ def createESN(inputSize, reservoirSize=None, spectralRadius=None,
         # degreeSparsity = randomState.choice([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
         degreeSparsity = randomState.choice([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8])
 
-    esnConfiguration = EsnConfiguration(reservoirSize, spectralRadius, degreeSparsity)
-    print(esnConfiguration)
-
-    return ESN(n_inputs=inputSize,
-               n_outputs=NUM_EMOTIONS,
-               n_reservoir=reservoirSize,
-               spectral_radius=spectralRadius,
-               sparsity=degreeSparsity,
-               out_activation= lambda x:x, #K.softmax,  # lambda x: x,  # logit logistic function ,
-               inverse_out_activation=lambda x: x,  # logit = inverse logistic function
-               random_state=randomState,
-               silent=True)
+    esn = ESN(n_inputs=inputSize,
+              n_outputs=outputSize,
+              n_reservoir=reservoirSize,
+              spectral_radius=spectralRadius,
+              sparsity=degreeSparsity,
+              out_activation=lambda x: x,  # K.softmax,  # lambda x: x,  # logit logistic function ,
+              inverse_out_activation=lambda x: x,  # logit = inverse logistic function
+              random_state=randomState,
+              silent=True)
+    esn.esnConfiguration = EsnConfiguration(reservoirSize, spectralRadius, degreeSparsity)
+    return esn
 
 
 class CESN:
-    def __init__(self, inputShape, randomGenerator):
-        # TODO: figure out the size of input fed to ESN
+    def __init__(self, inputShape, randomGenerator, outputSize):
         self.cnn = CNN(inputShape, randomGenerator)
-        self.esn = createESN(inputSize=self.cnn.outputSize, randomState=randomGenerator)
+        self.esn = createESN(inputSize=self.cnn.outputSize, outputSize=outputSize, randomState=randomGenerator)
 
     def fit(self, X, y):
         CNNProcessedInput = self.cnn.predict(X)
@@ -199,43 +211,97 @@ class CESN:
         return self.esn.predict(CNNProcessedInput)
 
 
-def createEnsembleClassifier(numClassifiers, inputSize, outputSize):
+def getRandomSeed(i):
+    return i * 50 + 10
+
+
+def createEnsembleClassifier(numClassifiers, outputSize):
     # classifiers = [createESN(inputSize, outputSize) for _ in range(numClassifiers)]
     classifiers = []
     for i in range(numClassifiers):
-        randomGenerator = np.random.RandomState(i * 42 + 7)  # Random formula
-        cesn = CESN((64, 64, 1), randomGenerator)
+        randomGenerator = np.random.RandomState(getRandomSeed(i))  # Random formula
+        cesn = CESN((64, 64, 1), randomGenerator, outputSize)
+
+        print("Classifier %d" % (i + 1))
+        for layerInfo in cesn.cnn.layersInfo:
+            print("\tCNN Layer: " + str(layerInfo))
+
+        print("\tESN: " + str(cesn.esn.esnConfiguration))
+        print()
+
         classifiers.append(cesn)
     return EnsembleClassifier(classifiers)
 
 
 def getEmotionFromFileName(imageName):
     person, emotionCode, photoNumber, extension = imageName.split('.')
-    for (i, potentialEmotion) in enumerate(EMOTIONS):
+    for (i, potentialEmotion) in enumerate(JAFFE_EMOTIONS):
         if potentialEmotion in emotionCode:
-            return CODED_EMOTIONS[i]
+            return JAFFE_CODED_EMOTIONS[i]
 
     raise Exception("No emotion matched the file %s" % imageName)
 
 
 def normalizeImage(image):
-    return image / 2 ** 16
-    # return (image - image.mean()) / (image.std() + 1e-8)
+    image = image / 2 ** 16
+    return (image - image.mean()) / (image.std() + 1e-8)
 
 
-def getJAFFEData(flatten=False):
+def getCKEmotion(personDir, emotionDir):
+    cohn_kanade_emotions_dir = utils.COHN_KANADE_PROCESSED_EMOTION_DIR
+    path = cohn_kanade_emotions_dir + '/' + personDir + '/' + emotionDir
+
+    try:
+        for name in os.listdir(path):
+            with open(path + '/' + name) as f:
+                line = f.readline()
+                emotionNum = int(float(line.strip()))
+                return CK_CODED_EMOTIONS[emotionNum - 1]
+    except FileNotFoundError:
+        return None
+
+    return None
+
+
+def getCohnKanadeData():
+    images = []
+    emotions = []
+    cohn_kanade_dir = utils.COHN_KANADE_PROCESSED_IMAGES_DIR
+
+    for personDir in os.listdir(cohn_kanade_dir):
+        person_full_dir = cohn_kanade_dir + '/' + personDir
+        for emotionDir in os.listdir(person_full_dir):
+            emotion_full_dir = person_full_dir + '/' + emotionDir
+            imagesNames = sorted(os.listdir(emotion_full_dir))
+
+            relevantImages = {imagesNames[0], imagesNames[-2], imagesNames[-1]}
+
+            emotion = getCKEmotion(personDir, emotionDir)
+            if emotion is None:
+                continue
+
+            for imageName in relevantImages:
+                images.append(getImage(emotion_full_dir + '/' + imageName))
+                emotions.append(emotion)
+
+    return np.array(images), np.array(emotions)
+
+
+def getJAFFEData():
     images = []
     emotions = []
     for imageName, fullPath in utils.getJAFFEImageNames():
-        image = normalizeImage(io.imread(fullPath))
-        if flatten:
-            image = image.flatten()
+        image = getImage(fullPath)
         images.append(image)
         emotions.append(getEmotionFromFileName(imageName))
 
     return np.array(images), np.array(emotions)
     # NUMDATA = 800
     # return np.array(images[:NUMDATA]), np.array(emotions[:NUMDATA])
+
+
+def getImage(fullPath):
+    return normalizeImage(io.imread(fullPath))
 
 
 RunInformation = namedtuple('RunInformation', ['numClassifiers', 'accuracy', 'timing'])
@@ -250,18 +316,26 @@ def timer(f):
     return result
 
 
-def doExperiments(minClassifiers=5, maxClassifiers=50):
-    images, y = getJAFFEData()
+def doExperiments(classifierRange=list(range(5, 51, 5)), useJaffe=False):
+    if useJaffe:
+        outputSize = JAFFE_NUM_EMOTIONS
+        images, y = getJAFFEData()
+    else:
+        outputSize = CK_NUM_EMOTIONS
+        images, y = getCohnKanadeData()
+
     images = images.reshape(images.shape[0], images.shape[1], images.shape[2], 1)
     X_train, X_test, y_train, y_test = train_test_split(
         images, y, test_size=0.2, random_state=42)
 
+    print(X_train.shape)
+
     runInfo = []
-    for numClassifiers in range(minClassifiers, maxClassifiers + 1, 5):
+    for numClassifiers in classifierRange:
         start = time()
         print("------- %d classifiers -------- " % numClassifiers)
         print("Creating and compiling models...")
-        ensembleClassifier = timer(lambda: createEnsembleClassifier(numClassifiers, 64 * 64, 1))
+        ensembleClassifier = timer(lambda: createEnsembleClassifier(numClassifiers, outputSize))
         print("Training models...")
         timer(lambda: ensembleClassifier.fit(X_train, y_train))
         print("Predicting values... ")
@@ -289,6 +363,6 @@ def getFreeFileName(startName, ext='csv'):
     return "%s%s.%s" % (startName, i, ext)
 
 
-runInfo = doExperiments(minClassifiers=5, maxClassifiers=50)
+runInfo = doExperiments(classifierRange=range(5, 25 + 1, 5))
 df = pd.DataFrame(runInfo)
 df.to_csv(getFreeFileName('results'))
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,21 +1,24 @@
 import shutil
 
 from skimage import io
-from skimage.color import rgb2gray
 from skimage.transform import resize
+from tqdm import tqdm
+
 import utils
 
 
 def transformImage(image):
-    greyImage = rgb2gray(image)
-    greyImageResized = resize(greyImage, (64, 64))
-    return greyImageResized
+    # greyImage = rgb2gray(image)
+    image_resized = resize(image, (64, 64))
+    return image_resized
 
 
 def transformAndSaveImage(name, datasetDir):
     fileNameWithoutExtension = name.rsplit('.', maxsplit=1)[0]
-    image = io.imread(utils.buildPath('datasets/%s/%s' % (datasetDir, name)))
+    image = io.imread(utils.buildPath('datasets/%s/%s' % (datasetDir, name)), as_grey=True)
     transformedImage = transformImage(image)
+
+    assert len(transformedImage.shape) == 2
 
     fullFilePath = utils.buildPath('datasets/transformed/%s/%s.png' %
                                    (datasetDir, fileNameWithoutExtension))
@@ -23,15 +26,14 @@ def transformAndSaveImage(name, datasetDir):
 
 
 def transformAllImages():
-    # for imageFileName, fullFileName in utils.getJAFFEImageNames(transformed=False):
-    #     transformAndSaveImage(imageFileName, datasetDir='JAFFE')
+    for imageFileName, fullFileName in tqdm(utils.getJAFFEImageNames(transformed=False)):
+        transformAndSaveImage(imageFileName, datasetDir='JAFFE')
 
-    for person, emotion, imageFileName, relFileName in utils.getCohnKanadeImageNames(transformed=False):
-        utils.createDirectoriesIfNeeded('%s/%s/%s/test.file' %
-                                        (utils.COHN_KANADE_PROCESSED_EMOTION_DIR, person, emotion))
+    for person, emotion, imageFileName, relFileName in tqdm(utils.getCohnKanadeImageNames(transformed=False)):
         utils.createDirectoriesIfNeeded('%s/%s/%s/test.file' %
                                         (utils.COHN_KANADE_PROCESSED_IMAGES_DIR, person, emotion))
         transformAndSaveImage(relFileName, datasetDir='CohnKanadePlus/cohn-kanade-images')
+
 
     # Copy emotions
     shutil.copytree(src=utils.COHN_KANADE_EMOTION_DIR, dst=utils.COHN_KANADE_PROCESSED_EMOTION_DIR)
