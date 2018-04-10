5540f59995b493fb1a5440506eb418d6c118d1fa
Gregory Bonaert <harokb@gmail.com>
2018-04-10 17:06:19 +0200

    Refactored model

--- src/model.py
+++ src/model.py
@@ -1,13 +1,12 @@
 from collections import namedtuple
 from time import time
 
-import os
 import numpy as np
 import pandas as pd
 
 from statistics import mean
 
-from sklearn.model_selection import StratifiedKFold, KFold
+from sklearn.model_selection import KFold
 
 from EnsembleClassifier import createEnsembleClassifier
 from keras import backend as K
@@ -15,7 +14,7 @@ from keras import backend as K
 # import matplotlib.pyplot as plt
 from data import getCohnKanadeData, getJAFFEData, JAFFE_NUM_EMOTIONS, CK_NUM_EMOTIONS, JAFFE_CODED_EMOTIONS, \
     CK_CODED_EMOTIONS
-from utils import timer, getAccuracy
+from utils import timer, getAccuracy, getFreeFileName
 from cnn import saveParams
 
 K.set_image_data_format('channels_last')
@@ -28,91 +27,94 @@ def getXandYSplits(X, y, n_splits=5):
     :return: trainXs, trainYs, testXs, testYs
     """
     skf = KFold(n_splits=n_splits)
-    trainXs, trainYs, testXs, testYs = [], [], [], []
-    for train, test in skf.split(X, y):
-        trainXs.append(X[train])
-        trainYs.append(y[train])
-        testXs.append(X[test])
-        testYs.append(y[test])
-
-    return trainXs, trainYs, testXs, testYs
+    return [(X[train], y[train], X[test], y[test]) for train, test in skf.split(X, y)]
 
 
-def doExperiments(resultsFileName, archFile, classifierRange=list(range(5, 51, 5)), useJaffe=False):
-    if useJaffe:
-        outputSize = JAFFE_NUM_EMOTIONS
-        oneHotEmotions = JAFFE_CODED_EMOTIONS
-        images, y = getJAFFEData(oneHotEncoded=False)
-    else:
-        outputSize = CK_NUM_EMOTIONS
-        oneHotEmotions = CK_CODED_EMOTIONS
-        images, y = getCohnKanadeData(oneHotEncoded=False)
+class Runner:
+    def __init__(self, classifierRange=list(range(5, 81, 5)), useJaffe=False):
+        self.classifiersStr = []
+        self.runInfo = []
+        self.classifierRange = classifierRange
+        self.useJaffe = useJaffe
 
-    images = images.reshape(images.shape[0], images.shape[1], images.shape[2], 1)
-    splittedData = trainXs, trainYs, testXs, testYs = getXandYSplits(images, y)
+        if self.useJaffe:
+            self.resultsFileName = getFreeFileName('resultsJaffe')
+            self.outputSize = JAFFE_NUM_EMOTIONS
+            self.oneHotEmotions = JAFFE_CODED_EMOTIONS
+            self.images, self.y = getJAFFEData(oneHotEncoded=False)
+        else:
+            self.resultsFileName = getFreeFileName('resultsCK')
+            self.outputSize = CK_NUM_EMOTIONS
+            self.oneHotEmotions = CK_CODED_EMOTIONS
+            self.images, self.y = getCohnKanadeData(oneHotEncoded=False)
 
-    print(trainXs[0].shape)
+        # Re-shape images to add a third value
+        self.images = self.images.reshape(self.images.shape[0], self.images.shape[1], self.images.shape[2], 1)
 
-    runInfo = []
-    randomGenerator = np.random.RandomState()
-    classifiersStr = []
+        # Create fileName
+        self.archFileName = self.resultsFileName.replace(".csv", ".arch")
+        self.paramsFileName = self.resultsFileName.replace(".csv", ".params")
 
-    for numClassifiers in classifierRange:
-        accuracies = []
-        start = time()
-        for i, (trainX, trainY, testX, testY) in enumerate(zip(*splittedData), start=1):
-            trainYEncoded = np.array([oneHotEmotions[i] for i in trainY])
-            testYEncoded = np.array([oneHotEmotions[i] for i in testY])
-            print("------- %d classifiers - split %d -------- " % (numClassifiers, i))
-            print("Creating and compiling models...")
-            ensembleClassifier = timer(lambda: createEnsembleClassifier(numClassifiers, outputSize, randomGenerator))
-            print("Training models and predicting values...")
-            predictions = timer(lambda: ensembleClassifier.fitAndPredict(trainX, trainYEncoded, testX))
-            print("Got predictions!")
-            accuracy = getAccuracy(predictions, testYEncoded)
-            accuracies.append(accuracy)
-            print("The accuracy for this split was: %.3f" % accuracy)
+        self.randomGenerator = np.random.RandomState()
 
-            classifiersStr.append(str(ensembleClassifier))
+        with open(self.paramsFileName, 'w') as paramsFile:
+            saveParams(paramsFile)
 
-            # Output architecture for the i-th split
-            archFile.write(str(ensembleClassifier) + '\n\n\n')
-            archFile.flush()
+        with open(self.archFileName, 'w') as archFile:
+            self.archFile = archFile
+            self.run()
 
-        # Do stats
-        end = time()
-        timing = end - start
-        averageAccuracy = mean(accuracies)
-        runInfo.append(RunInformation(numClassifiers, round(averageAccuracy, 4), round(timing, 4)))
+        self.archFile = None
 
-        # Output results
-        df = pd.DataFrame(runInfo)
-        df.to_csv(resultsFileName)
+    def run(self):
+        splittedData = trainXs, trainYs, testXs, testYs = getXandYSplits(self.images, self.y)
+        print(trainXs[0].shape)
 
-        print("The accuracy gotten with %d classifiers is %.3f" % (numClassifiers, averageAccuracy))
-        print()
-        print()
+        for numClassifiers in self.classifierRange:
+            self.accuracies = []
+            start = time()
+            for i, (trainX, trainY, testX, testY) in enumerate(splittedData, start=1):
+                self.trainAndTest(trainX, trainY, testX, testY, i, numClassifiers)
 
-    print(runInfo)
-    return runInfo, classifiersStr
+            # Do stats
+            end = time()
+            timing = end - start
+
+            averageAccuracy = mean(self.accuracies)
+            information = RunInformation(numClassifiers, round(averageAccuracy, 4), round(timing, 4))
+            self.runInfo.append(information)
+
+            # Output results
+            self.saveCurrentResults()
+
+            print("The accuracy gotten with %d classifiers is %.3f" % (numClassifiers, averageAccuracy))
+            print()
+            print()
+        print(self.runInfo)
+
+    def saveCurrentResults(self):
+        df = pd.DataFrame(self.runInfo)
+        df.to_csv(self.resultsFileName)
+
+    def trainAndTest(self, trainX, trainY, testX, testY, i, numClassifiers):
+        trainYEncoded = np.array([self.oneHotEmotions[i] for i in trainY])
+        testYEncoded = np.array([self.oneHotEmotions[i] for i in testY])
+        print("------- %d classifiers - split %d -------- " % (numClassifiers, i))
+        print("Creating and compiling models...")
+        ensembleClassifier = timer(
+            lambda: createEnsembleClassifier(numClassifiers, self.outputSize, self.randomGenerator))
+        print("Training models and predicting values...")
+        predictions = timer(lambda: ensembleClassifier.fitAndPredict(trainX, trainYEncoded, testX))
+        print("Got predictions!")
+        accuracy = getAccuracy(predictions, testYEncoded)
+        self.accuracies.append(accuracy)
+        print("The accuracy for this split was: %.3f" % accuracy)
+        self.classifiersStr.append(str(ensembleClassifier))
+        # Output architecture for the i-th split
+        self.archFile.write(str(ensembleClassifier) + '\n\n\n')
+        self.archFile.flush()
 
 
-def getFreeFileName(startName, ext='csv'):
-    i = 0
-    while os.path.exists("%s%s.%s" % (startName, i, ext)):
-        i += 1
-
-    return "%s%s.%s" % (startName, i, ext)
-
-
-useJaffe = True
-if useJaffe:
-    fileName = getFreeFileName('resultsJaffe')
-else:
-    fileName = getFreeFileName('resultsCK')
-
-archFileName = fileName.replace(".csv", ".arch")
-paramsFileName = fileName.replace(".csv", ".params")
-with open(archFileName, 'w') as archFile, open(paramsFileName, 'w') as paramsFile:
-    saveParams(paramsFile)
-    runInfo, classifiersStr = doExperiments(fileName, archFile, classifierRange=range(5, 80 + 1, 5), useJaffe=useJaffe)
+useJaffe = False
+classifierRange = list(range(5, 81, 5))
+runner = Runner(classifierRange=classifierRange, useJaffe=useJaffe)
