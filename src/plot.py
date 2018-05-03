0ceab0be7ca114f58556817dbbe8dcf80cbdae9d
Gregory Bonaert <harokb@gmail.com>
2018-05-03 21:14:51 +0200

    Improved plotting

--- src/plot.py
+++ src/plot.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Plot Cohn-Kanade predictions
-dfCK = pd.read_csv("resultsCK0.csv")
+dir = "remoteResults/"
+PDtype = "PD"
+dfCK = pd.read_csv(dir + "resultsCK" + PDtype + "0.csv")
 plt.plot(dfCK["numClassifiers"], dfCK["accuracy"], 'r.-')
 
-dfJAFFE = pd.read_csv("resultsJaffe0.csv")
+dfJAFFE = pd.read_csv(dir + "resultsJaffe" + PDtype + "0.csv")
 plt.plot(dfJAFFE["numClassifiers"], dfJAFFE["accuracy"], 'b.-')
 
 
-plt.axis([0, 50, 0.4, 0.95])
+plt.axis([0, 85, 0.1, 0.95])
 plt.ylabel("Accuracy")
 plt.xlabel("Ensemble size")
-plt.savefig("plot.png")
+plt.savefig("plot" + PDtype + ".png")
