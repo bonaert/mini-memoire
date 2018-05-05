ee4a5ec57d36cf1546ab75a115a617a6d3917d4c
Gregory Bonaert <harokb@gmail.com>
2018-05-06 00:48:38 +0200

    Make plots and export them easily

---
+++ src/makeAndExportPlots.sh
@@ -0,0 +1,3 @@
+python3 plot.py
+cp plotPI.png "$HOME/Dropbox/aULB/Ba3/mini-memoire/rapport/figures/"
+cp plotPD.png "$HOME/Dropbox/aULB/Ba3/mini-memoire/rapport/figures/"
--- src/plot.py
+++ src/plot.py
@@ -2,16 +2,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Plot Cohn-Kanade predictions
-dir = "remoteResults/"
-PDtype = "PD"
-dfCK = pd.read_csv(dir + "resultsCK" + PDtype + "0.csv")
-plt.plot(dfCK["numClassifiers"], dfCK["accuracy"], 'r.-')
+def makePlot(PDtype, CKnum, JaffeNum):
+	dir = "remoteResults/"
+	dfCK = pd.read_csv(dir + "resultsCK" + PDtype + str(CKnum) + ".csv")
+	plt.plot(dfCK["numClassifiers"], dfCK["accuracy"], 'r.-')
+	
+	dfJAFFE = pd.read_csv(dir + "resultsJaffe" + PDtype + str(JaffeNum) + ".csv")
+	plt.plot(dfJAFFE["numClassifiers"], dfJAFFE["accuracy"], 'b.-')
 
-dfJAFFE = pd.read_csv(dir + "resultsJaffe" + PDtype + "0.csv")
-plt.plot(dfJAFFE["numClassifiers"], dfJAFFE["accuracy"], 'b.-')
+	plt.legend(['CK+ dataset', 'JAFFE dataset'])
 
+	plt.axis([0, 85, 0, 0.95])
+	plt.ylabel("Accuracy")
+	plt.xlabel("Ensemble size")
+	plt.savefig("plot" + PDtype + ".png")
+	plt.close()
 
-plt.axis([0, 85, 0.1, 0.95])
-plt.ylabel("Accuracy")
-plt.xlabel("Ensemble size")
-plt.savefig("plot" + PDtype + ".png")
+makePlot("PI", 1, 1)
+makePlot("PD", 0, 0)
