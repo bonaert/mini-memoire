9d82441489f27a024cface3498e08da4c8bd4edd
Gregory Bonaert <harokb@gmail.com>
2018-05-06 16:09:59 +0200

    plot.py: reformatted file and put output in figures folder

--- src/plot.py
+++ src/plot.py
@@ -1,22 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Plot Cohn-Kanade predictions
 def makePlot(PDtype, CKnum, JaffeNum):
-	dir = "remoteResults/"
-	dfCK = pd.read_csv(dir + "resultsCK" + PDtype + str(CKnum) + ".csv")
-	plt.plot(dfCK["numClassifiers"], dfCK["accuracy"], 'r.-')
-	
-	dfJAFFE = pd.read_csv(dir + "resultsJaffe" + PDtype + str(JaffeNum) + ".csv")
-	plt.plot(dfJAFFE["numClassifiers"], dfJAFFE["accuracy"], 'b.-')
+    dir = "remoteResults/"
+    dfCK = pd.read_csv(dir + "resultsCK" + PDtype + str(CKnum) + ".csv")
+    plt.plot(dfCK["numClassifiers"], dfCK["accuracy"], 'r.-')
 
-	plt.legend(['CK+ dataset', 'JAFFE dataset'])
+    dfJAFFE = pd.read_csv(dir + "resultsJaffe" + PDtype + str(JaffeNum) + ".csv")
+    plt.plot(dfJAFFE["numClassifiers"], dfJAFFE["accuracy"], 'b.-')
 
-	plt.axis([0, 85, 0, 0.95])
-	plt.ylabel("Accuracy")
-	plt.xlabel("Ensemble size")
-	plt.savefig("plot" + PDtype + ".png")
-	plt.close()
+    plt.legend(['CK+ dataset', 'JAFFE dataset'])
+
+    plt.axis([0, 85, 0, 0.95])
+    plt.ylabel("Accuracy")
+    plt.xlabel("Ensemble size")
+    plt.savefig("figures/plot" + PDtype + ".png")
+    plt.close()
+
 
 makePlot("PI", 1, 1)
 makePlot("PD", 0, 0)
